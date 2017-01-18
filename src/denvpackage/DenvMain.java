4b496df3884553fa13b4eb630c1cc4aca708956d
Denis Ryabykh <denisryabykh@gmail.com>
2017-01-18 22:50:40 +0200

    adding files

---
+++ src/denvpackage/DenvMain.java
@@ -0,0 +1,13 @@
+package denvpackage;
+import java.util.Scanner;
+/**
+ * Created by DELL on 1/16/2017.
+ */
+public class DenvMain {
+        public static void main (String [] args){
+            Scanner scan = new Scanner(System.in);
+            System.out.println(scan.nextLine());
+            System.out.print("https://github.com/sp1r1n/javacore");
+        }
+    }
+
