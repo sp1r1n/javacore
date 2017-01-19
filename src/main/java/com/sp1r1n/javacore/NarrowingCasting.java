8dacf59d656f76933d52bb3b7835da98693cd2a7
Denis Ryabykh <denisryabykh@gmail.com>
2017-01-19 22:07:12 +0200

    update runner

--- src/main/java/com/sp1r1n/javacore/NarrowingCasting.java
+++ src/main/java/com/sp1r1n/javacore/NarrowingCasting.java
@@ -4,12 +4,12 @@ package com.sp1r1n.javacore;
  * Created by DELL on 1/19/2017.
  */
 public class NarrowingCasting {
-    int int1 = 127;
-    float float1 = 0.0f;
+    int int1 = 0;
+    float float1 = 3.14159f;
 
     public void narrowing(){
-        float1 = int1;
-        System.out.println(float1);
+        int1 = (int) float1;
+        System.out.println(int1);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/sp1r1n/javacore/Runner.java
@@ -0,0 +1,11 @@
+package com.sp1r1n.javacore;
+
+/**
+ * Created by DELL on 1/19/2017.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        NarrowingCasting casting = new NarrowingCasting();
+        casting.narrowing();
+    }
+}
