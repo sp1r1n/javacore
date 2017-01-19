9758aa6d92d467a3ab0492ff52cd153ef81b2800
Denis Ryabykh <denisryabykh@gmail.com>
2017-01-19 21:08:30 +0200

    maven

---
+++ src/main/java/com/sp1r1n/javacore/App.java
@@ -0,0 +1,13 @@
+package com.sp1r1n.javacore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
