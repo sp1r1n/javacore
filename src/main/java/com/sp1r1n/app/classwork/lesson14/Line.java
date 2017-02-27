e644dd27ea3c6cd5ea3c063372f310c685d00096
Denis Ryabykh <denisryabykh@gmail.com>
2017-02-27 22:05:01 +0200

    lesson14 start

--- src/main/java/com/sp1r1n/app/classwork/lesson14/Line.java
+++ src/main/java/com/sp1r1n/app/classwork/lesson14/Line.java
@@ -3,15 +3,38 @@ package com.sp1r1n.app.classwork.lesson14;
 /**
  * Created by DELL on 2/27/2017.
  */
-public class Line extends Pixel{
+public class Line extends Pixel {
 
-    private int polyLine[];
+    Pixel[] pixelArray = new Pixel[3];
+    int i = 0;
 
-    public Line(int x, int y){
+    public Line(int x, int y) {
         super(x, y);
     }
 
-    public static void main(String[] args) {
-
+    public void addPixel(Pixel pixel) {
+        for (; i < pixelArray.length; i++) {
+            pixelArray[i] = pixel;
+        }
     }
+
+    public int[] getCoordinates() {
+        int[] res = new int[pixelArray.length * 2];
+        for (Pixel pixel: pixelArray){
+            System.out.print(getCoordinates());
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Pixel pixel = new Pixel(10, 20);
+        Pixel pixel2 = new Pixel(15, 20);
+        Pixel pixel3 = new Pixel(13, 20);
+
+        Line l = new Line(10, 20);
+        l.addPixel(pixel);
+        l.addPixel(pixel2);
+        l.addPixel(pixel3);
+    }
+
 }
